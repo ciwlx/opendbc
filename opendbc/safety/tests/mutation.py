9aeea97343a9999f07503c6d5bcd6a56f37d7ed8
ciwlx <21superyoung@naver.com>
2026-05-29 19:33:06 +0900

    Comment out logical operations in mutation.py

--- opendbc/safety/tests/mutation.py
+++ opendbc/safety/tests/mutation.py
@@ -47,9 +47,8 @@ MUTATOR_FAMILIES = {
   # "arithmetic_assignment": ("assignment_expression", {"+=": "-=", "-=": "+=", "*=": "/=", "/=": "*=", "%=": "*="}),
   # "arithmetic": ("binary_expression", {"+": "-", "-": "+", "*": "/", "/": "*", "%": "*"}),
   # "remove_negation": ("unary_expression", {"!": ""}),
-  "logical_opposite": ("binary_expression", {"&&": "||", "||": "&&"}),
-  # "logical_right": ("binary_expression", {"&&": ",", "||": ","}),
-  # "logical_same": ("binary_expression", {"&&": "==", "||": "=="}),
+  # "logical_opposite": ("binary_expression", {"&&": "||", "||": "&&"}),
+  "logical_bitwise": ("binary_expression", {"&&": "&", "||": "|"}),
   # "logical_different": ("binary_expression", {"&&": "!=", "||": "!="}),
 }
 
